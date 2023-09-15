9890152e17e4e1cd9cf43f5a9601984b35c56656
Kumar Chaudhary <kumarbhetwal25@gmail.com>
2023-09-15 20:26:40 +0545

    ffff

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import Button from '@/components/ui/Button'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <div>
+    <Button/>
+
+  </div>
+}
+
+export default page
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,35 @@
+import { cva } from 'class-variance-authority'
+import { ButtonHTMLAttributes, FC } from 'react'
+
+interface ButtonProps {
+  
+}
+
+const buttonVariance = cva(
+    'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none',
+    {
+        variants: {
+          variant: {
+            default: 'bg-slate-900 text-white hover:bg-slate-800',
+            ghost: 'bg-transparent hover:text-slate-900 hover:bg-slate-200',
+          },
+          size: {
+            default: 'h-10 py-2 px-4',
+            sm: 'h-9 px-2',
+            lg: 'h-11 px-8',
+          },
+        },
+        defaultVariants: {
+          variant: 'default',
+          size: 'default',
+        },
+      }
+    )
+
+    export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{}
+
+const Button: FC<ButtonProps> = ({}) => {
+  return <div>Button</div>
+}
+
+export default Button
