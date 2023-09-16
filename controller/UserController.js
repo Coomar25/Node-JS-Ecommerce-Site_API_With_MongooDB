cf3139069e940f7473b473429c1df9e23e54e556
Kumar Chaudhary <kumarbhetwal25@gmail.com>
2023-09-16 21:04:58 +0545

    Insert operation optimized

--- controller/UserController.js
+++ controller/UserController.js
@@ -2,16 +2,28 @@ import User from "../models/userModel.js"
 
 
 export const createUser = async (req, res) => {
-    const email = req.body.email;
-    const findUser = await User.find({email: email});
-    if(!findUser){
-        const newUser = User.create(req.body);
-        res.json(newUser);
-    }else{
-        res.json({
-            message: "User already exists",
+    try {
+        const email = req.body.email;
+        const existingUser = await User.findOne({ email: email });
+        if (existingUser) {
+            return res.status(409).json({
+                message: "User already exists",
+                success: false,
+            });
+        }
+        // Create a new user
+        const newUser = await User.create(req.body);
+        return res.status(201).json({
+            message: "User created successfully",
+            success: true,
+            user: newUser,
+        });
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(500).json({
+            message: "Internal server error",
             success: false,
-        })
+        });
     }
 };
 
